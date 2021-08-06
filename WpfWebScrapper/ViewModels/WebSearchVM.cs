9b03515cbaae64949350f844cbf0aa35cfd4f103
bipeenj@gmail.com <bipeenj@gmail.com>
2021-08-07 01:39:15 +1000

    initial commit

---
+++ WpfWebScrapper/ViewModels/WebSearchVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfWebScrapper.ViewModels
+{
+    public class WebSearchVM
+    {
+        public WebSearchVM()
+        {
+            KeyWords = "conveyancing software";
+            URL = "www.smokeball.com.au";
+        }
+        public string KeyWords { get; set; }
+        public string URL { get; set; }
+
+        public List<KeywordVM> Result { get; set; }
+    }
+}
