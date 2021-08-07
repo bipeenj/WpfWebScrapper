58d1dc16bb4827d9880a664f9e29cb42fe365747
bipeenj@gmail.com <bipeenj@gmail.com>
2021-08-07 23:02:23 +1000

    Initial UI changes

--- WpfWebScrapper/ViewModels/WebSearchVM.cs
+++ WpfWebScrapper/ViewModels/WebSearchVM.cs
@@ -1,21 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.ObjectModel;
 
 namespace WpfWebScrapper.ViewModels
 {
-    public class WebSearchVM
+    public class WebSearchVM: INotifyPropertyChanged
     {
+        private ObservableCollection<KeywordVM> m_Result;
+        private string m_Strings;
         public WebSearchVM()
         {
+            Result = new ObservableCollection<KeywordVM>();
             KeyWords = "conveyancing software";
             URL = "www.smokeball.com.au";
+            
+            
+            
         }
-        public string KeyWords { get; set; }
+        public string KeyWords 
+        {
+            get { return m_Strings; }
+            set
+            {
+                m_Strings = value;
+                OnPropertyChanged("KeyWords");
+                Result.Clear();
+                m_Strings.Split(";").ToList().ForEach(word => {
+                    if (word.Any())
+                    {
+                        Result.Add(new KeywordVM
+                        {
+                            Name = word,
+                            Count = 0
+                        });
+                    }
+                });
+                OnPropertyChanged("Result");
+            }
+        }
         public string URL { get; set; }
 
-        public List<KeywordVM> Result { get; set; }
+        public ObservableCollection<KeywordVM> Result 
+        {
+            get { return m_Result; }
+            set
+            {
+                m_Result = value;
+                OnPropertyChanged("Result");
+                
+            }
+        }
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        
     }
 }
