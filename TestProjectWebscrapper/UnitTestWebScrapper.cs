895ca7032568a54c2d5b490c984c10f2bfc5216d
bipeenj@gmail.com <bipeenj@gmail.com>
2021-08-08 22:17:08 +1000

    trying to use appium for automation UI testing

--- TestProjectWebscrapper/UnitTestWebScrapper.cs
+++ TestProjectWebscrapper/UnitTestWebScrapper.cs
@@ -1,13 +1,25 @@
 using NUnit.Framework;
 using WpfWebScrapper.Models;
 using System.Linq;
+using System.Windows.Automation;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Appium;
+using System;
+
 namespace TestProjectWebscrapper
 {
     public class Tests
     {
+        public const string DriverUrl = "http://127.0.0.1:4723";
+        public WindowsDriver<WindowsElement> DesktopSession;
         [SetUp]
         public void Setup()
         {
+            //AppiumOptions Options = new AppiumOptions();
+            //Options.AddAdditionalCapability("app", "..\\WpfWebScrapper\\bin\\Debug\\netcoreapp3.1\\WpfWebScrapper.exe");
+            //Options.AddAdditionalCapability("deviceName", "WindowsPC");
+            //DesktopSession = new WindowsDriver<WindowsElement>(new Uri(DriverUrl), Options);
+            //Assert.IsNotNull(DesktopSession);
         }
 
         [Test]
@@ -18,6 +30,18 @@ namespace TestProjectWebscrapper
             var result = engine.SearchWithinText(testContent, "https ://www.smokeball.com.au");
             Assert.IsTrue( result.Count() == 1);
             
+        }
+        [Test]
+        public void Test2()
+        {
+            
+            Assert.Pass();
+
+        }
+        [TearDown]
+        public void Close()
+        {
+            DesktopSession?.CloseApp();
         }
     }
 }
